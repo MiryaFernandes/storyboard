47975b2fb513817e13de5308776d49bf249ef13b
MIRYÃ ELIZABETH FERNANDES BORGES <21276382@CFP127ALUNO.LOCAL>
2022-02-04 17:02:49 -0300

    projeto iniciado 04/02

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>projeto php</title>
+    <link rel="stylesheet" href="./css/sytle.css">
+</head>
+<body>
+    <nav class="dp-menu">
+    <ul>
+        <li><a href="#"> home </a></li>
+        <li><a href="#"> projetos </a></li>
+        <ul>
+            <li><a href="#"> media </a></li>
+            <li><a href="#"> calculadora </a></li>
+            <li><a href="#"> tabuada </a></li>
+            <li><a href="#"> pares e impares </a></li>
+        </ul>
+        <li><a href="#"> quem somos</a></li>
+    </ul>
+    
+</body>
+</html>
